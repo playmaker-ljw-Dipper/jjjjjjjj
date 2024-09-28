d53dda22759f199adecc7be8edfef8588cb35962
playmaker-ljw-Dipper <150129316+playmaker-ljw-Dipper@users.noreply.github.com>
2024-09-28 21:09:05 +0800

    add one change

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "First we serve our club member , then our supporter, finaly the whole world ";
 
 const About = () => {
   return (
